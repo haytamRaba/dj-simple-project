a3e2a360c330af4c6d854c1dccee70eb3eb4437d
haytam <haytamraba11@gmail.com>
2025-05-28 22:48:18 +0100

    1. creating an app 2. (include)-views- (urls_app)

---
+++ myproject/myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+
+# Create your views here.
+
+def index(response):
+  return HttpResponse("<h2>hey!<h2>")
+def index2(respose):
+  return HttpResponse("second page !!!!!!!!!!!")
